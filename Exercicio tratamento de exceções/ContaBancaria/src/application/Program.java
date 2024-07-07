7448f16a78e61a1b42773ebc59a2bcd1c7334ce6
João Loquingen <vichtor_jovem@hotmail.com>
2024-07-07 20:35:54 -0300

    Adicionando exercicio tratamento de exceções

---
+++ Exercicio tratamento de exceções/ContaBancaria/src/application/Program.java
@@ -0,0 +1,45 @@
+package application;
+
+import entities.Account;
+import exceptions.BusinessException;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account data");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Holder: ");
+        String holder = sc.nextLine();
+        System.out.print("Initial balance: ");
+        Double balance = sc.nextDouble();
+        sc.nextLine();
+        System.out.print("Withdraw limit: ");
+        Double withdrawLimit = sc.nextDouble();
+        sc.nextLine();
+
+        Account acc = new Account(number, holder, balance, withdrawLimit);
+
+        System.out.println();
+        System.out.print("Enter amount for withdraw: ");
+        Double amount = sc.nextDouble();
+
+        try {
+            acc.withdraw(amount);
+            System.out.print("New balance: " + acc.getBalance());
+        } catch (BusinessException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+
+        sc.close();
+    }
+}
---
+++ Exercicio tratamento de exceções/ContaBancaria/src/exceptions/BusinessException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+import java.io.Serial;
+
+public class BusinessException extends RuntimeException{
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public BusinessException (String msg){
+        super(msg);
+    }
+}
